afd834e4a87c11e6c3d5b8965f8ac5c9c332cb78
thehunk1206 <khantauhid313@gmail.com>
2022-09-26 20:01:56 +0530

    added script to create tensorflow project dir structure

---
+++ create-project-dir-structure.sh
@@ -0,0 +1,14 @@
+# Create a directory structure for a new TensorFlow project
+!/bin/zsh
+
+echo "Creating directory structure for new TensorFlow project"
+
+mkdir -p models && touch models/__init__.py
+mkdir -p layers && touch layers/__init__.py
+mkdir -p losses && touch losses/__init__.py
+mkdir -p utils && touch utils/__init__.py
+mkdir -p configs && touch configs/__init__.py
+mkdir -p outputs
+mkdir -p logs
+
+echo "Directory structure created"
